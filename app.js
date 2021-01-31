5f80f039d51a969e5df26eb0495223696cf9dd8d
Amirhossein <ahshadravan@gmail.com>
2021-01-31 14:52:20 +0330

    Nodemailer is added

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+
+// View engine setup
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, "views"));
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.render('contact', {layout: false});
+  });
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Detail</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Company: ${req.body.company}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+    // async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "mail.shadravann.ir",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: '', // generated ethereal user
+        pass: '', // generated ethereal password
+      },
+      tls: {
+          rejectUnauthorized: false
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Nodemailer Contact 👻" <me@shadravann.ir>', // sender address
+      to: "ahshadravan@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: output // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', {msg: 'Email has been sent'})
+  }
+
+  main().catch(console.error);
+
+});
+
+
+app.listen(3000, () => console.log('Server started....'));
